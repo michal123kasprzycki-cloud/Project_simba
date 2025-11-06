ebe17a651fc4e9d96930387731dced6cb29c4865
michal123kasprzycki-cloud <michal.123kasprzycki@gmail.com>
2025-11-06 19:03:02 +0100

    Update Simba-software.cpp

--- Simba-software.cpp
+++ Simba-software.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -10,28 +10,28 @@ private:
         uint8_t mask;
     };
 
-    vector <Prefix> z;
+    vector <Prefix> IP_vector;
 
 public:
 
     bool add(const uint32_t& base, const uint8_t maskLength) {
         if (maskLength > 32) { return false; }
-        for (auto x : z) {
+        for (auto x : IP_vector) {
             if (base == x.ip && maskLength == x.mask) {
                 return false;
             }
         }
-        z.push_back({ base,maskLength });
+        IP_vector.push_back({ base,maskLength });
         return true;
     }
 
     bool del(const uint32_t& base, const uint8_t maskLength) {
         if (maskLength > 32) { return false; }
-        for (int i = 0; i < z.size(); i++) {
-            if (z[i].ip == base && z[i].mask == maskLength) {
-                swap(z[i].ip, z.back().ip);
-                swap(z[i].mask, z.back().mask);
-                z.pop_back();
+        for (int i = 0; i < IP_vector.size(); i++) {
+            if (IP_vector[i].ip == base && IP_vector[i].mask == maskLength) {
+                swap(IP_vector[i].ip, IP_vector.back().ip);
+                swap(IP_vector[i].mask, IP_vector.back().mask);
+                IP_vector.pop_back();
                 return true;
             }
         }
@@ -40,10 +40,10 @@ public:
 
     int check(const uint32_t& ip) {
         int result = -1;
-        for (int i = 0; i < z.size(); i++) {
-            uint32_t mask = (z[i].mask == 0 ? 0 : ~0U << (32 - z[i].mask));
-            if ((ip & mask) == (z[i].ip & mask)) {
-                result = (z[i].mask > result ? z[i].mask : result);
+        for (int i = 0; i < IP_vector.size(); i++) {
+            uint32_t mask = (IP_vector[i].mask == 0 ? 0 : ~0U << (32 - IP_vector[i].mask));
+            if ((ip & mask) == (IP_vector[i].ip & mask)) {
+                result = (IP_vector[i].mask > result ? IP_vector[i].mask : result);
             }
         }
         return result;
@@ -51,30 +51,7 @@ public:
 
 };
 
-
-
-
 int main()
 {
-
     return 0;
 }
-
-
-
-
-
-
-
-
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
